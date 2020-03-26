baba268288dae32012e1e7d1e02c246a94bc1242
cat <gaumeochit162@gmail.com>
2020-03-26 22:44:17 +0700

    WIP Record and save speech

--- src/ex01.py
+++ src/ex01.py
@@ -1,13 +1,18 @@
 import librosa
 import soundfile as sf
 import sounddevice as sd
+from scipy.io.wavfile import write
 import numpy as np
 import queue
 import sys
 import threading
+import speech_recognition as sr
 
 FILE_APPEND = "sentences"
 OUTPUT_DIR = "./output"
+SAMPLE_RATE = 44100
+MAXIMUM_DURATION = 3600
+CHANNELS = 2
 
 #Read files
 file = open('speech.txt', 'r')
@@ -23,9 +28,9 @@ for sc in file_content:
         file_extracted_content.append(s.strip())
 file_extracted_content = [s for s in file_extracted_content if s]
 
-print("\n")
 print("Extracted {} sentences from file.".format(len(file_extracted_content)))
 print("Default output prefix is " + FILE_APPEND + "_#.wav\n")
+print("___________________________")
 print("\n")
 
 inputQueue = queue.Queue()
@@ -46,6 +51,11 @@ def mainRecording():
         i += 1
         print("Output: " + file_name)
         input("Press any key to start recording.\n")
+        recording = sd.rec(int(MAXIMUM_DURATION * SAMPLE_RATE), samplerate = SAMPLE_RATE, channels = CHANNELS)
+        sd.wait()
+        #
+        #Save as WAV file 
+        write(file_name, SAMPLE_RATE, recording)
     return
 
 mainRecording()
