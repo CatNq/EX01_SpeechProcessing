894423b4d7ae7b49d8f0b7eadac308ceded1e4c9
cat <gaumeochit162@gmail.com>
2020-03-26 17:39:13 +0700

    read from file

--- src/ex01.py
+++ src/ex01.py
@@ -6,4 +6,25 @@ import queue
 import sys
 import threading
 
-print("SpeechProcessing_EX01\n")
+FILE_APPEND = "sentences"
+OUTPUT_DIR = "./output"
+
+#Read files
+text_file = open('speech.txt', 'r')
+text_content = text_file.readlines()
+
+#Format the sentences
+text_extracted_content = []
+
+#Remove and cut blank spaces between each sentences
+for sc in text_content:
+    sentences = sc.split('.')
+    for s in sentences:
+        text_extracted_content.append(s.strip())
+text_extracted_content = [s for s in text_extracted_content if s]
+
+print("Extracted {} sentences from file.".format(len(text_extracted_content)))
+
+print("Default output prefix is " + FILE_APPEND + "_#.wav")
+
+print("Outputting to " + OUTPUT_DIR + "/" + FILE_APPEND + "_#.wav\n")
